344a209338456d8ef3bd0652b14670922ea5735d
Lancer He <lancer.he@gmail.com>
2015-02-24 13:18:15 +0800

    Update gitignore.

---
+++ application/services/mock/Generator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Service\Mock;
+
+class Generator {
+
+    protected $_uri = [];
+
+    protected $_mock = [];
+
+    protected $_output = [];
+
+    public function __construct($uri_id) {
+        $Model = new \Model_Uri();
+        $this->_uri   = $Model->fetchRowById($uri_id);
+
+        $Model = new \Model_Mock();
+        $this->_mock  = $Model->fetchListByUriId($uri_id);
+    }
+
+    public function generate() {
+        foreach ($this->_mock as $idx => $row) {
+            $Mock = new \Service\Mock();
+            $Mock->init($row, $this->_uri);
+
+            $this->__rebuild($Mock);
+        }
+
+        $this->__output();
+    }
+
+    private function __rebuild( \Service\Mock $Mock ) {
+        $this->_output[] = [
+            "request" => [
+                "query" => $Mock->getRequestQuery(),
+                "post"  => $Mock->getRequestPost(),
+            ],
+            "response" => [
+                "delay"  => $Mock->getTimeout(),
+                "header" => $Mock->getResponseHeader(),
+                "body"   => $Mock->getResponseBody(),
+            ],
+        ];
+    }
+
+    private function __output() {
+        $output = "exports.mock = " . json_encode($this->_output);
+        $output_file = ROOT_PATH. "/mock" . $this->_uri['uri'] . ".js";
+        $dirname = pathinfo($output_file)['dirname'];
+        if ( ! is_dir( $dirname) ) mkdir($dirname, 0775, true);
+        file_put_contents($output_file, $output);
+    }
+}
---
+++ application/services/mock/output/HTTPProtocol.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Service\Mock\Output;
+
+use Service\Mock;
+
+class HTTPProtocol {
+
+    protected $_Mock = null;
+
+    protected $_output = '';
+
+    public function __construct(Mock $Mock) {
+        $this->_Mock = $Mock;
+    }
+
+    public function output() {
+        $this->_buildRequestString();
+        $this->_buildResponseString();
+        return $this->_output;
+    }
+
+    protected function _buildRequestString() {
+        $uri = $this->_Mock->getUri();
+        $request_body_string = '';
+        $method        = $this->_Mock->getRequestMethod();
+        $request_query = $this->_Mock->getRequestQuery();
+        $request_post  = $this->_Mock->getRequestPost();
+        if ( ! empty( $request_query ) ) {
+            $uri .= "?" . http_build_query( $request_query );
+        }
+        if ( ! empty( $request_post ) ) {
+            $method = "POST";
+            $request_body_string = http_build_query( $request_post );
+        }
+        $this->_output .= 
+            "> $method $uri HTTP/1.1\r\n".
+            "> HOST: " . APPLICATION_MOCKSERVER_HOST . "\r\n".
+            "> Accept: */*\r\n".
+            "> Content-Length: " . strlen($request_body_string) . "\r\n" . 
+            "> \r\n" . 
+            "$request_body_string\r\n";
+    }
+
+    protected function _buildResponseString() {
+        $this->_output .= 
+            "< HTTP/1.1 200 OK\r\n";
+
+        foreach ($this->_Mock->getResponseHeader() as $key => $value) {
+            $this->_output .= "< $key: $value\r\n";
+        }
+        $this->_output .= 
+            "< \r\n" . 
+            $this->_Mock->getResponseBody();
+    }
+}
